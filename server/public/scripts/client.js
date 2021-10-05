0bbba15b36d6368dcd0d31f92fa15a17677e5495
Amanda Zimmerman <amanda.zimms@gmail.com>
2021-10-05 10:32:15 -0500

    in progress of setting up client.js, including stretch goals

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,6 +6,7 @@ $( document ).ready( function(){
   setupClickListeners()
   // load existing koalas on page load
   getKoalas();
+  $( '#viewKoalas' ).on( 'click', '#readyForTransferButton', readyForTransfer );//Capture click event of 
 
 }); // end doc ready
 
@@ -25,16 +26,85 @@ function setupClickListeners() {
     // call saveKoala with the new obejct
     saveKoala( koalaToSend );
   }); 
+  $( '#viewKoalas').on('click', '.removeKoalaButton', removeKoala);
 }
 
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
   
+  $.ajax({
+    method: 'GET',
+    url: '/koalas'
+
+  }).then (function(response) {
+    console.log('response:', response);
+    let viewKoalas = $('#viewKoalas');
+    viewKoalas.empty();
+    for(let i=0; i<response.length; i++){
+      viewKoalas.append(
+        `<tr>
+          <td data-id='${response[i].id}'>${response[i].id}</td>
+          <td>${response[i].name}</td>
+          <td>${response[i].age}</td>
+          <td>${response[i].gender}</td>
+          <td>${response[i].ready_for_transfer}</td> 
+          <td>${response[i].notes}</td>
+          <td><button class='readyForTransferButton' data-id='${response[i].id}'>Ready for Transfer</button></td>
+          <td><button class='removeKoalaButton' data-id='${response[i].id}'>Remove</button></td>
+        </tr>`
+      );
+    }
+
+  }).catch( function (err) {
+    console.log('error getting koalas:', err);
+    alert('Error getting koalas - see console');
+  })
 } // end getKoalas
+
+function removeKoala() {
+  let koalaToRemove = $(this).data('id');
+
+  $.ajax({
+    method: 'DELETE',
+    url: '/koalas?id=' + koalaToRemove
+  }).then ( function ( response ){
+    console.log ( 'remove Koala:', response);
+    
+  }).catch( function ( err ){
+    console.log ( err );
+    alert('Oops! There was an error with your delete. Check Console for details.');
+  });
+  
+}// end removeKoala
 
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
   // ajax call to server to get koalas
- 
+  $.ajax('/koalas',(req, res)=>{
+    let queryText = 'INSERT INTO koalas (name, age, gender, ready_for_transfer, notes) VALUES ($1, $2, $3, $4, $5)';
+    let values = [req.body.name, req.body.age, req.body.gender,req.body.ready_for_transfer, req.body.notes];
+
+    pool.query(queryText, values).then((results)=>{
+      res.sendStatus(201);
+
+    }).catch((err)=>{
+      res.sendStatus(500);
+      alert(`error adding koala`);
+      console.log(err);
+    })
+  })
+}
+//Update koala ready for transfer
+function readyForTransfer() {
+  console.log( `in readyForTransfer` );
+  $.ajax({
+    method: 'PUT',
+    url: '/koalas?id=' + $( this ).data( 'id' )//telling server which record to update
+  }).then( function( response ){
+    console.log( `back from update:`, response );
+  }).catch( function( err ){
+    alert( `error with update `);
+    console.log( err );
+  })
 }
